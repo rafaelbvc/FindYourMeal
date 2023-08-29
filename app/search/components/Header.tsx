b864a3d525b60775a83a409b13f260a7e22f38f0
Rafael Vendramini <72450490+rafaelbvc@users.noreply.github.com>
2023-08-29 00:54:51 -0300

    v. 0.5.2- HotFix

--- app/search/components/Header.tsx
+++ app/search/components/Header.tsx
@@ -1,7 +1,6 @@
-import SearchBar from "../../components/searchBar";
+import SearchBar from "../../components/SearchBar";
 
 const Header = () => {
-
   return (
     <div className="bg-gradient-to-r from-[#5f6984] to-[#0f1f47]  p-2">
       <SearchBar />
